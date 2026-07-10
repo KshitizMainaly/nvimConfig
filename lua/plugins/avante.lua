d7d8b8a457b8725d715822476aec9ab55670be4e
KshitizMainaly <kshitizmainaly3334@gmail.com>
2026-07-10 21:24:10 +0545

    Add avante.nvim (AI assistant with OpenRouter) + dressing.nvim

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,53 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  build = "make",
+  opts = {
+    provider = "openrouter",
+    providers = {
+      openrouter = {
+        __inherited_from = "openai",
+        endpoint = "https://openrouter.ai/api/v1",
+        model = "anthropic/claude-sonnet-4",
+        api_key_name = "OPENROUTER_API_KEY",
+        headers = {
+          ["HTTP-Referer"] = "http://localhost",
+          ["X-Title"] = "Neovim AI",
+        },
+        extra_request_body = {
+          temperature = 0.7,
+          max_tokens = 8192,
+        },
+      },
+    },
+    behaviour = {
+      auto_suggestions = false,
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = true,
+    },
+    mappings = {
+      ask = "<leader>aa",
+      edit = "<leader>ae",
+      refresh = "<leader>ar",
+      toggle = {
+        debug_prefix = "<leader>ad",
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "stevearc/dressing.nvim",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = { file_types = { "markdown", "Avante" } },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
