66b7483a6cfe04ddef5dd1793cd6190d9f99e2f5
KshitizMainaly <kshitizmainaly3334@gmail.com>
2026-07-13 12:00:59 +0545

    Use Ctrl+J for newline in Avante/dressing inputs (Shift+Enter = same as Enter in terminals)

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -4,15 +4,17 @@ return {
   config = function(_, opts)
     require("dressing").setup(opts)
 
+    local function insert_newline()
+      local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+      vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "" })
+      vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
+    end
+
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = "DressingInput",
-      desc = "Shift+Enter inserts a newline in dressing inputs",
+      pattern = { "DressingInput", "AvanteInput" },
+      desc = "Ctrl+j inserts a newline in input buffers",
       callback = function()
-        vim.keymap.set("i", "<S-CR>", function()
-          local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-          vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "" })
-          vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
-        end, { buffer = true, desc = "Insert newline (Shift+Enter)" })
+        vim.keymap.set("i", "<C-j>", insert_newline, { buffer = true, desc = "Insert newline (Ctrl+J)" })
       end,
     })
   end,
